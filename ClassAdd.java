856741fc05c6daf1c026f6c65ce83bb2616596e4
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-05 11:51:00 +0900

    ClassAdd

---
+++ ClassAdd.java
@@ -0,0 +1,27 @@
+package test.class1;
+
+public class ClassAdd {
+	private int result;
+	private int n1,n2;
+	
+	public int getN1() {
+		return n1;
+	}
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+	public int getResult() {
+		return result;
+	}
+	
+	public void addNum() {
+		result = n1 + n2;
+	}
+
+}
