3961ebf29d8cf88164d1a4c5f90b7d8b300e7989
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-07 10:56:00 +0900

    Circle.this

--- Circle.java
+++ Circle.java
@@ -1,28 +1,37 @@
-package test.class1;
+package test.this1;
 
 public class Circle {
-	
 	private int r;
-	private double circum,area;
-	public int getR() {
-		return r;
-	}
-	public void setR(int r) {
-		this.r = r;
-	}
+	private double circum, area;
+	
 	public double getCircum() {
 		return circum;
 	}
+
 	public double getArea() {
 		return area;
 	}
 	
+	public int getR() {
+		return r;
+	}
+
+	public Circle(int r) {
+		this.r = r;
+	}
+
 	public void calcCircum() {
 		circum = 2 * Math.PI * r;
 	}
 	
 	public void calcArea() {
-		area = Math.PI * Math.pow(r, 2);
+		area = Math.pow(r, 2) * Math.PI;
+	}
+	
+	public void showInfo() {
+		calcCircum();
+		calcArea();
+		RunCircle.showPrint(this);
 	}
 
 }
