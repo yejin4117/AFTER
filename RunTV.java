bf61debf91e63c7c7642befc8674939b0fd079be
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-07 11:48:49 +0900

    RunTV.this

--- RunTV.java
+++ RunTV.java
@@ -1,55 +1,9 @@
-package test.class1;
+package test.this1;
 
 public class RunTV {
 
 	public static void main(String[] args) {
-		TV tv1 = new TV();
-		TV tv2 = new TV();
-		TV tv3 = new TV();
-		
-		tv1.setProducer("LG");
-		tv1.setColor("White");
-		tv1.setSize(55);
-		
-		tv2.setProducer("SAMSUNG");
-		tv2.setColor("Black");
-		tv2.setSize(65);
-		
-		tv3.setProducer("APPLE");
-		tv3.setColor("Silver");
-		tv3.setSize(77);
-		
-		System.out.println("==============================");
-		System.out.println(tv1.getProducer() + " TV");
-		System.out.println(tv1.getSize() + " 인치");
-		System.out.println(tv1.getColor() + " 색상");
-		tv1.powerOn();
-		for(int i=0; i<7; i++) {
-			tv1.upChannel();
-		}
-		for(int i=0; i<10; i++) {
-			tv1.upVolume();
-		}
-		
-		System.out.println("==============================");
-		
-		System.out.println("==============================");
-		System.out.println(tv3.getProducer() + " TV");
-		System.out.println(tv3.getSize() + " 인치");
-		System.out.println(tv3.getColor() + " 색상");
-		tv3.powerOn();
-		for(int i=0; i<11; i++) {
-			tv3.upChannel();
-		}
-		for(int i=0; i<5; i++) {
-			tv3.upVolume();
-		}
-		
-		tv3.downChannel();
-		tv3.downChannel();
-		
-		System.out.println("==============================");
-
+		TV tv1 = new TV(55,"Black", "LG전자");
 
 	}
 
