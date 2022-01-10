0b2f78252d350dd13b4e9bbd2ada0459361ce7a4
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-10 12:12:56 +0900

    Child.final

---
+++ Child.java
@@ -0,0 +1,13 @@
+package test.final1;
+
+//부모클래스가 final이면 상속이 금지 된다.
+//부모클래스의 메소드가 final이면 메소드 재정의(overriding)가 금지된.
+
+public class Child extends Parent {
+	
+//	@Override
+//	public void rest() {
+//		System.out.println("1시간 휴식한다.");
+//	}
+
+}
