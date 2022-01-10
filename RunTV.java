f89347636d0158817b78ee52e3a5a6be24656e8d
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-10 12:12:23 +0900

    RunTV.field

--- RunTV.java
+++ RunTV.java
@@ -1,10 +1,15 @@
-package test.this1;
+package test.field1;
 
 public class RunTV {
 
 	public static void main(String[] args) {
-		TV tv1 = new TV(55,"Black", "LG전자");
-
-	}
+		TV t1 = new TV("Black", 55);
+		TV t2 = new TV("Silver", 65);
+		TV t3 = new TV("Blue", 100);
+		
+		System.out.printf("색상 : %s, 크기 : %d, 제조사 : %s\n", t1.color, t1.size, t1.PRODUCER);
+		System.out.printf("색상 : %s, 크기 : %d, 제조사 : %s\n", t2.color, t2.size, t2.PRODUCER);
+		System.out.printf("색상 : %s, 크기 : %d, 제조사 : %s\n", t3.color, t3.size, t3.PRODUCER);
+	}//main
 
 }
