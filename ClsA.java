08ed2e04f9940db18de6113f1b3412b593062255
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-11 10:42:03 +0900

    ClsA

---
+++ ClsA.java
@@ -0,0 +1,12 @@
+package test.constructorcall;
+
+public class ClsA {
+	public ClsA(){
+		System.out.println("ClsA의 기본생성자입니다.");
+	}
+	
+	public ClsA(int n) {
+		System.out.println("ClassA(int n)의 생성자 호출");
+	}
+
+}
