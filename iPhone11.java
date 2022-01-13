fc60e06d8b775b255ea61a63afe0a58edeff4527
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-13 09:26:39 +0900

    iPhone11.smartphone

---
+++ iPhone11.java
@@ -0,0 +1,51 @@
+package test.smartphone;
+
+import test.phone.interface1.MP3;
+import test.phone.interface1.MobilePhone;
+
+public class iPhone11 implements MobilePhone, MP3 {
+
+	@Override
+	public String searchMusic(String searchWords) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void playMusic(String searchWords) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopMusic() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendCall(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveCall(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendSMS(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveSMS(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
