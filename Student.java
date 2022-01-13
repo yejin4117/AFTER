e7b7d17139849abd464bff80a867416bc4724005
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-13 11:52:54 +0900

    Student.(mvc.score.model)

--- Student.java
+++ Student.java
@@ -1,16 +1,64 @@
-package test.inheritance;
+package mvc.score.model;
 
-public class Student extends Person {
-	int stuID;	//학번
-	int grade;	//학년
-	int ban;	//반
+public class Student {
+
+	private String name;
+	private int stuId;
+	private int kor, eng, math, science, history;
 	
-	public void study() {
-		System.out.println(name + "(이)가 공부한다.");
+	public Student(String name, int stuId, int kor, int eng, int math, int science, int history) {
+		this.name = name;
+		this.stuId = stuId;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.science = science;
+		this.history = history;
 	}
 	
-	public void play() {
-		System.out.println(name + "(이)가 논다.");
+	public String getName() {
+		return name;
 	}
-
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getScience() {
+		return science;
+	}
+	public void setScience(int science) {
+		this.science = science;
+	}
+	public int getHistory() {
+		return history;
+	}
+	public void setHistory(int history) {
+		this.history = history;
+	}
+	
+	
+	
 }
