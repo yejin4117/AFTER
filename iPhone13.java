477b08592b51e0743d3d51b08f1b69f072c1d766
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-13 09:27:03 +0900

    iPhone13.smartphone

---
+++ iPhone13.java
@@ -0,0 +1,61 @@
+package test.smartphone;
+
+import test.phone.interface1.SmartPhone;
+
+public class iPhone13 implements SmartPhone {
+
+	@Override
+	public void sendCall(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveCall(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendSMS(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveSMS(String phoneNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String searchMusic(String searchWords) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void playMusic(String searchWords) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopMusic() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void installApp(String appName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void runApp(String appName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
