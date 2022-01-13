127a1b517ff58a0ef0a034fb5a043b3c184ea824
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2022-01-13 09:25:16 +0900

    MobilePhone.interface

---
+++ MobilePhone.java
@@ -0,0 +1,10 @@
+package test.phone.interface1;
+
+public interface MobilePhone {
+
+	void sendCall (String phoneNumber);
+	void receiveCall (String phoneNumber);
+	void sendSMS (String phoneNumber);
+	void receiveSMS (String phoneNumber);
+
+}
